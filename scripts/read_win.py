cdbc914567093f759f212da1cc006263d1af9bfb
Michal Kocur <michal.kocur@stuba.sk>
2022-01-17 09:27:21 +0100

    po prvom teste

---
+++ scripts/read_win.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+import rospy
+
+form std_msgs.msg import Header
+
+def callback(msg):
+        rospy.loginfo(rospy.get_caller_id() + "")
+  
+if __name__ == '__main__':
+    try:
+
+        rospy.init_node('scan_header')
+        sub = rospy.Subscriber("/msg_header",std_msgs::Header,callback)
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
